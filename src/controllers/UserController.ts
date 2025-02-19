a58f684a5dd85c3447af4491cb2d60e8c35bd982
pwtpcn <palmdz08@gmail.com>
2025-02-19 13:57:32 +0700

    feat(User): now can create user, get all user, get user by id

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,6 @@
+import { User } from "@prisma/client";
 import Elysia, { t } from "elysia";
+import UserRepository from "src/repositories/UserRepository.server";
 
 const UserController = new Elysia({
   prefix: "/api/user",
@@ -6,9 +8,81 @@ const UserController = new Elysia({
 });
 
 UserController.model({
-    User: t.Object({
-        
-    })
-})
+  User: t.Object({
+    username: t.String(),
+    email: t.String(),
+    password: t.String(),
+    profile_image_url: t.String(),
+    salt: t.String(),
+    createdAt: t.Date(),
+    updatedAt: t.Date(),
+  }),
+});
+
+UserController.get(
+  "/getAll",
+  async () => {
+    const userRepository = new UserRepository();
+    const users: User[] = await userRepository.getAllUsers();
+    return users;
+  },
+  {
+    detail: {
+      summary: "Get All User", //API Name
+      description: "Get all user from database", //API Description
+    },
+  }
+);
+
+UserController.get(
+  "/get/:id",
+  async ({ params: { id } }) => {
+    const userRepository = new UserRepository();
+    const user: User | null = await userRepository.getUserByID(id);
+    return user ?? { error: "User not found", status: 200 };
+  },
+  {
+    params: t.Object({ id: t.String() }),
+    detail: {
+      summary: "Get User By ID",
+      description: "Get user by id from database",
+    },
+  }
+);
+
+UserController.post(
+  "/create",
+  async ({ body }) => {
+    const userRepository = new UserRepository();
+    try {
+      body.salt === undefined
+        ? (body.salt = Math.random().toString(36).substring(2, 12))
+        : ""; //generate random
+      const newBody = { ...body, salt: body.salt };
+      const password = await Bun.password.hash(
+        newBody.password + newBody.salt,
+        "bcrypt"
+      ); //hash password
+      newBody.password = password; //set password
+      const user: User = await userRepository.createUser(newBody);
+      return user;
+    } catch (error: any) {
+      return { error: error.message };
+    }
+  },
+  {
+    body: t.Object({
+      username: t.String(),
+      email: t.String(),
+      password: t.String(),
+      salt: t.Optional(t.String()),
+      createdAt: t.Date(),
+    }),
+    detail: {
+        summary: "Create new User",
+        description: "Create new User in deatabase"
+    }
+  }
+);
 
 export default UserController;
--- src/repositories/UserRepository.server.ts
+++ src/repositories/UserRepository.server.ts
@@ -1,21 +1,72 @@
+import { User } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import db from "src/repositories/database.server";
 
-interface CreateProps {
-  username: string;
-  email: string;
-  password: string;
-}
-
 class UserRepository {
-  public static async create({ username, email, password }: CreateProps) {
-    const user = await db.user.create({
-      data: {
-        username: username,
-        email: email,
-        password: password,
-      },
+  public async createUser({
+    username,
+    email,
+    password,
+    salt,
+    createdAt,
+  }: {
+    username: string;
+    email: string;
+    password: string;
+    salt: string;
+    createdAt: Date;
+  }): Promise<User> {
+    try {
+      const response = await db.user.create({
+        data: {
+          username: username,
+          email: email,
+          password: password,
+          salt: salt,
+          createdAt: createdAt,
+        },
+      });
+      return response;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        switch (error.code) {
+          case "P2002":
+            throw new Error("Email already exists");
+          default:
+            throw new Error("Internal Server Error");
+        }
+      }
+    }
+    throw new Error("Internal Server Error");
+  }
+
+  public async getUserByID(uuid: string): Promise<User | null> {
+    return await db.user.findUnique({
+      where: { uuid },
     });
-    return user;
+  }
+
+  public async getAllUsers(): Promise<User[]> {
+    return await db.user.findMany();
+  }
+
+  public async deleteUser(id: string): Promise<User | null> {
+    try {
+      const response = await db.user.delete({
+        where: { uuid: id },
+      });
+      return response;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        switch (error.code) {
+          case "P2025":
+            throw new Error("Record does not exists.");
+          default:
+            throw new Error(error.code);
+        }
+      }
+    }
+    throw new Error("Internal Server Error");
   }
 }
 
