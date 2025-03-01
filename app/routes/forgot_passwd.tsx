851eaf6c16ee3aaf68bc83f543ad118bbe8b6e29
Thanapoom.C <palmdz08@gmail.com>
2025-03-01 23:30:17 +0700

    add session loader in forgot password page

--- app/routes/forgot_passwd.tsx
+++ app/routes/forgot_passwd.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Link, redirect, useLoaderData } from "@remix-run/react";
 import { HomeIcon } from "lucide-react";
+import { getSession } from "~/utils/session.server";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,10 +10,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader({ request }: { request: Request }) {
+  const session = await getSession(request);
+  const user = session.get("user");
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  return {
+    message: "",
+    user: user,
+  };
+}
+
 export default function Index() {
+    const { user } = useLoaderData<typeof loader>();
+
   return (
     <div className="bg-[#FFF0D1] h-screen flex flex-col justify-center items-center gap-3">
-      <h1 className="text-[#664343] font-bold">Forgot Password Page</h1>
+      <h1 className="text-[#664343] font-bold">Hello {user.username}</h1>
+      <h1 className="text-[#664343] font-bold">This is forgot password page</h1>
       <div className="flex flex-row gap-3">
         <h1 className="text-[#664343] font-bold">Go back to home page</h1>
         <Link to="/" prefetch="render">
